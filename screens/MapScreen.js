a70b80bdfda7549e4c47a8b4b915f195e5c059b4
Ilyas Arafath <dev2@scientiaindia.com>
2021-05-27 01:00:26 +0530

    Google map additional features added

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,54 +1,168 @@
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, PermissionsAndroid } from 'react-native'
+import MapView, { PROVIDER_GOOGLE,Marker } from 'react-native-maps'
+import Geolocation from '@react-native-community/geolocation';
 
-import { StyleSheet, View } from 'react-native';  
-import MapView from 'react-native-maps';  
-import { Marker } from 'react-native-maps';  
-import Geolocation from "react-native-geolocation-service";
-import React, { useState, useEffect } from 'react';
+const MapScreen = () => {
 
-function MapScreen({ navigation }) {
-    return (
-      <View style={styles.MainContainer}>  
-  
-      <MapView  
-        style={styles.mapStyle}  
-        showsUserLocation={true}  
-        zoomEnabled={true}  
-        zoomControlEnabled={true}  
-        initialRegion={{  
-          latitude: 28.579660,   
-          longitude: 77.321110,  
-          latitudeDelta: 0.0922,  
-          longitudeDelta: 0.0421,  
-        }}>  
+  const [mapWidth, setMapWidth] = useState('99%')
+  const [
+    currentLongitude,
+    setCurrentLongitude
+  ] = useState(0);
+  const [
+    currentLatitude,
+    setCurrentLatitude
+  ] = useState(0);
+  const [
+    locationStatus,
+    setLocationStatus
+  ] = useState('');
 
-        <Marker  
-          coordinate={{ latitude: 28.579660, longitude: 77.321110 }}  
-          title={"JavaTpoint"}  
-          description={"Java Training Institute"}  
-        />  
-      </MapView>  
+  // Update map style to force a re-render to make sure the geolocation button appears
+  const updateMapStyle = () => {
+    setMapWidth('100%')
+  }
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      if (Platform.OS === 'ios') {
+        subscribeLocationLocation();
+      } else {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              title: 'Location Access Required',
+              message: 'This App needs to Access your location',
+            },
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //To Check, If Permission is granted
+            subscribeLocationLocation();
+          } else {
+            setLocationStatus('Permission Denied');
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      }
+    };
+    requestLocationPermission();
+    return () => {
+      Geolocation.clearWatch(watchID);
+    };
+  }, []);
+
+
+  const getOneTimeLocation = () => {
+    setLocationStatus('Getting Location ...');
+    Geolocation.getCurrentPosition(
+      //Will give you the current location
+      (position) => {
+        setLocationStatus('You are Here');
+
+        //getting the Longitude
+        const currentLongitude = position.coords.longitude;
         
-    </View>  
+        //getting the Latitude
+        const currentLatitude = position.coords.latitude;
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+        
+        //Setting Longitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      (error) => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 30000,
+        maximumAge: 1000
+      },
     );
-    }
+  }
 
-    export default MapScreen;
 
-    const styles = StyleSheet.create({  
-      MainContainer: {  
-        position: 'absolute',  
-        top: 0,  
-        left: 0,  
-        right: 0,  
-        bottom: 0,  
-        alignItems: 'center',  
-        justifyContent: 'flex-end',  
-      },  
-      mapStyle: {  
-        position: 'absolute',  
-        top: 0,  
-        left: 0,  
-        right: 0,  
-        bottom: 0,  
-      },  
-    });  
+  const subscribeLocationLocation = () => {
+    watchID = Geolocation.watchPosition(
+      (position) => {
+        //Will give you the location on location change
+        
+        setLocationStatus('You are Here');
+        console.log(position);
+
+        //getting the Longitude     
+        const currentLongitude = position.coords.longitude;
+
+        //getting the Latitude 
+        const currentLatitude = position.coords.latitude;
+
+        //Setting Longitude state
+        setCurrentLongitude(currentLongitude);
+
+        //Setting Latitude state
+        setCurrentLatitude(currentLatitude);
+      },
+      (error) => {
+        setLocationStatus(error.message);
+      },
+      {
+        enableHighAccuracy: false,
+        maximumAge: 1000
+      },
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        mapType='standard'
+        customMapStyle={googleMapStyle}
+        style={[styles.map, { width: mapWidth }]}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        // zoomEnabled={true}  
+        zoomControlEnabled={true}
+        showsCompass={true}
+        onMapReady={() => {
+          getOneTimeLocation()
+          updateMapStyle()
+        }}
+        // 
+      >
+        <Marker  
+          coordinate={{ latitude: currentLatitude, longitude: currentLongitude }}  
+          title={"You are here"}  
+          description={""}  
+        />
+
+      </MapView>
+    </View>
+  )
+}
+
+const googleMapStyle = [{
+  featureType: "administrative",
+  elementType: "geometry",
+  stylers: [{
+    visibility: "off"
+  }]
+}]
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  map: {
+    height: '100%'
+  }
+})
+
+export default MapScreen
